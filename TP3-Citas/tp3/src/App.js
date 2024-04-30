3b59a50ecd8ad9865e4f999b28ae98b3fd4291f3
Danamandelbaum <128741083+Danamandelbaum@users.noreply.github.com>
2024-04-30 15:01:31 -0300

    Add files via upload

---
+++ TP3-Citas/tp3/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Titulo from './components/Titulo/titulo.js';
+import Form from './components/Form/form.js';
+import Citas from './components/Citas/citas.js';
+
+function App() {
+  return (
+    <div>
+      <Titulo />
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">
+            <Citas />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
